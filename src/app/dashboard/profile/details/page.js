8fc7d95cb687c89a9d04abce00b256a7a1a8ca13
munir <smsirajulmonir@gmail.com>
2026-05-30 13:38:09 +0600

    basic routing explored

---
+++ src/app/dashboard/profile/details/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Details = () => {
+    return (
+        <div>
+            <h1>Details</h1>
+            <p>This is your details page.</p>
+        </div>
+    );
+};
+
+export default Details;
